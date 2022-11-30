a6d114b90e3d87f13784143a21c1f4a7377dad12
llpupp <llpupp@hotmail.com>
2022-11-30 17:47:57 -0300

    Via Cep Service

--- src/main/java/one/digitalinnovation/gof/service/ViaCepService.java
+++ src/main/java/one/digitalinnovation/gof/service/ViaCepService.java
@@ -6,7 +6,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import one.digitalinnovation.gof.model.Endereco;
 
-@FeignClient(name = "viacep", url = "https://viacep.com.br/ws")
+/**
+ * Client HTTP, criado via <b>OpenFeign</b>, para o consumo da API do
+ * <b>ViaCEP</b>.
+ *
+ * @see <a href="https://spring.io/projects/spring-cloud-openfeign">Spring Cloud OpenFeign</a>
+ * @see <a href="https://viacep.com.br">ViaCEP</a>
+ */
+
+ @FeignClient(name = "viacep", url = "https://viacep.com.br/ws")
 public interface ViaCepService {
 
     @GetMapping("/{cep}/json/")
